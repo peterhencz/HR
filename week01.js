3e36f6f29b26cde78460f5c64670dc864ea56b70
Peter Hencz <peter.hencz@gmail.com>
2019-02-24 23:36:23 +0100

    save

--- week01.js
+++ week01.js
@@ -7,18 +7,15 @@ function breakingRecords(games, score) {
   for (let i = 0; i < games; i++) {
     let lastScore = score[i];
     if (lastScore < minScore) {
-      console.log(minScore, lastScore);
       min.push(minScore);
       minScore = lastScore;
     }
     if (lastScore > maxScore) {
-      console.log(maxScore, lastScore);
       max.push(maxScore);
       maxScore = lastScore;
     }
   }
-  console.log("max: ", max.length, max);
-  console.log("min: ", min.length, min);
+  console.log(max.length, min.length);
 }
 
 const games1 = 10;
@@ -28,5 +25,6 @@ const score1 = [3, 4, 21, 36, 10, 28, 35, 5, 24, 42];
 const games2 = 9;
 const score2 = [10, 5, 20, 20, 4, 5, 2, 25, 1];
 // 2 4
+
 breakingRecords(games1, score1);
 breakingRecords(games2, score2);
