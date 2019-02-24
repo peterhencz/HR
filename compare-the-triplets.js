6f10d54bf47516050d4b2d33f160c9a16abe524a
Peter Hencz <peter.hencz@gmail.com>
2019-02-25 00:31:07 +0100

    comparetriplets

---
+++ compare-the-triplets.js
@@ -0,0 +1,28 @@
+// https://www.hackerrank.com/contests/mindtech-trial/challenges/compare-the-triplets
+
+function compareTriplets(a, b) {
+  let aResults = 0;
+  let bResults = 0;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] > b[i]) {
+      aResults++;
+    }
+    if (a[i] < b[i]) {
+      bResults++;
+    }
+  }
+  console.log(aResults, bResults);
+}
+
+const a1 = [5, 6, 7];
+const b1 = [3, 6, 10];
+// 1 1
+
+const a2 = [17, 28, 30];
+const b2 = [99, 16, 8];
+// 2 1
+
+console.log("first");
+compareTriplets(a1, b1);
+console.log("second");
+compareTriplets(a2, b2);
---
+++ drawing-book.js
@@ -0,0 +1,7 @@
+//https://www.hackerrank.com/contests/mindtech-trial/challenges/drawing-book
+
+function pageCount() {
+  console.log("cica");
+}
+
+pageCount();
