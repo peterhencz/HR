d3dbf504a5b950e3dedab00740a08512b940286c
Peter Hencz <peter.hencz@gmail.com>
2019-04-08 12:14:30 +0200

    save

--- drawing-book.js
+++ drawing-book.js
@@ -1,1 +1,16 @@
 // https://www.hackerrank.com/contests/mindtech-trial/challenges/drawing-book
+
+function pageCount(n, p) {
+  console.log("cica");
+}
+
+n1 = 6;
+p1 = 2;
+// 1
+
+n2 = 5;
+p2 = 4;
+// 0
+
+pageCount(n1, p1);
+pageCount(n2, p2);
